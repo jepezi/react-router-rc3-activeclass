353c1b0454541a8b0ce81e9bd0bc80ca3e715835
Jirat Arinrith <jepezi@gmail.com>
2015-10-09 22:05:41 +0700

    first

---
+++ history.js
@@ -0,0 +1,5 @@
+import { useBasename } from 'history'
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import createMemoryHistory from 'history/lib/createMemoryHistory';
+
+export default createBrowserHistory();
---
+++ index.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Router, IndexLink, Link } from 'react-router';
+import history from './history';
+
+
+class App extends Component {
+  render() {
+    return <div>
+      <h1>App</h1>
+      <ul>
+        <li><IndexLink id="overviewLink" to="/" activeClassName="active">Home</IndexLink></li>
+        <li><Link id="contactLink" to="/product_category" activeClassName="active">Category</Link></li>
+        <li><Link id="productsLink" to="/product_category/new" activeClassName="active">New Category</Link></li>
+        <li><IndexLink id="productsIndexLink" to="/product_category/1" activeClassName="active">Category 1</IndexLink></li>
+        <li><Link id="specificProductLink" to="/login" activeClassName="active">login</Link></li>
+      </ul>
+      {this.props.children}
+    </div>;
+  }
+}
+
+class Wrapper extends Component {
+  render() {
+    return <div>wrapper {this.props.children}</div>
+  }
+}
+
+class Home extends Component {
+  render() {
+    return <div>home</div>
+  }
+}
+
+class ProductCategory extends Component {
+  render() {
+    return <div>product category main {this.props.children}</div>
+  }
+}
+
+class ProductCategoryForm extends Component {
+  render() {
+    console.warn(this.props);
+    return <div>product category {this.props.params.id ? this.props.params.id : 'new'}</div>
+  }
+}
+
+class Login extends Component {
+  render() {
+    return <div>Login</div>
+  }
+}
+
+class NotFound extends Component {
+  render() {
+    return <div>Not found</div>
+  }
+}
+
+const routes =
+{component: App, childRoutes: [
+  {path: '/', component: Wrapper, indexRoute: Home, childRoutes: [
+    {path: 'product_category', component: ProductCategory, childRoutes: [
+      {path: 'new', component: ProductCategoryForm},
+      {path: ':id', component: ProductCategoryForm},
+    ]},
+  ]},
+  {path: '/login', component: Login},
+  {path: '*', component: NotFound},
+]};
+
+render(
+  <Router history={history} routes={routes}/>,
+  document.getElementById('root')
+);
